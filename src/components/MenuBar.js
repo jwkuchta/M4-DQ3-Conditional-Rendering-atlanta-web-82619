54fdf0273d77f317aeb9b91c16ea3214e7ba4226
Justina Kuchta <justyna.kuchta@gmail.com>
2019-12-24 09:08:22 -0500

    Done.

--- src/components/MenuBar.js
+++ src/components/MenuBar.js
@@ -1,38 +1,89 @@
 import React from 'react'
 
-const MenuBar = (props) => {
+const MenuBar = ({select, selected}) => {
 
-  /*
-
-  The 'a' tags below are the menu items. Think about the way a menu 
-  should work. When you click a menu item, the button typically becomes
-  'active' to indicate that it is currently selected. How could we achieve
-  this programatically? What other behavior do we expect when we click
-  on a menu item? Do we need state in this component, and if not, how can
-  this component be made aware of what is currently the active menu item?
-
-  */
+ const handleClick = (e) => {
+   select(e.target.id)
+  //  e.target.className = "item active"
+  //  if(e.target.className==="item") {
+  //    e.target.className = "item active"
+  //  }
+  //  e.target.className = "item"
+  // this did not work becauce it was changing the className
+  // of the <i> element instead of the <a> element
+ }
 
   return (
     <div className="ui four item menu">
-      <a className="item active" id="profile">
+
+      <a onClick={handleClick} 
+      className={"item " + (selected==="profile" ? "active" : null)} 
+      id="profile">
         <i className="user large icon" id="profile"/>
       </a>
 
-      <a className="item" id="photo">
+      <a onClick={handleClick} 
+      className={"item " + (selected==="photo" ? "active" : null)}
+      id="photo">
         <i className="photo large icon" id="photo"/>
       </a>
 
-      <a className="item" id="cocktail">
+      <a onClick={handleClick} 
+      className={"item " + (selected==="cocktails" ? "active" : null)}
+      id="cocktail">
         <i className="cocktail large icon" id="cocktail"/>
       </a>
 
-      <a className="item" id="pokemon"> 
-        <i className=" themeisle large icon" id="pokemon"/>
+      <a onClick={handleClick} 
+      className={"item " + (selected==="pokemon" ? "active" : null)}
+      id="pokemon"> 
+        <i className="themeisle large icon" id="pokemon"/>
       </a>
+
     </div>
   )
-
 }
 
 export default MenuBar
+
+// import React from 'react'
+
+
+// const MenuBar = ({selected, select}) => {
+
+//   const handleClick = (event) => {
+//     select(event.target.id)
+//   }
+
+//   return (
+//     <div className="ui four item menu">
+//       <a className={"item " + (selected==="profile" ? "active" : null)} 
+//          id="profile"
+//          onClick={handleClick}>
+//         <i className="user large icon" id="profile"/>
+//       </a>
+
+//       <a className={"item " + (selected==="photo" ? "active" : null)} 
+//          id="photo"
+//          onClick={handleClick}>
+//         <i className="photo large icon" id="photo"/>
+//       </a>
+
+//       <a className={"item " + (selected==="cocktail" ? "active" : null)} 
+//          id="cocktail"
+//          onClick={handleClick}>
+//         <i className="cocktail large icon" id="cocktail"/>
+//       </a>
+
+//       <a className={"item " + (selected==="pokemon" ? "active" : null)} 
+//          id="pokemon"
+//          onClick={handleClick}> 
+//         <i className=" themeisle large icon" id="cocktail"/>
+//       </a>
+//     </div>
+//   )
+
+// }
+
+// export default MenuBar
+
